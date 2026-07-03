c07a1a6da15846152f93b818c8f6990b57959873
MohanLakshmiReddy <mohanlakshmireddy.k@gmail.com>
2026-07-03 08:30:49 +0530

    Add files via upload

---
+++ SL4J_Logging_exercises/src/test/java/com/example/sl4j_logging_exercises/Sl4JLoggingExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sl4j_logging_exercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sl4JLoggingExercisesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
